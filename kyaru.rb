d93755e638d6597a3e9c2fb42635dd8a95b04e59
TAKANO Mitsuhiro <takano32@gmail.com>
2019-09-12 23:43:47 +0900

    config.yml がないときの処理を修正

--- kyaru.rb
+++ kyaru.rb
@@ -1,15 +1,17 @@
 require 'discordrb'
 require 'yaml'
 
-# yaml 形式の設定ファイルを読み込む
-config = YAML.load_file("config.yml")
-# 設定ファイルがなかったら環境変数を読み込む
 discord_token = ''
-if config['discord_token']
+
+begin
+  # yaml 形式の設定ファイルを読み込む
+  config = YAML.load_file("config.yml")
   discord_token = config['discord_token']
-else
-  discord_token = ENV['DISCORD_TOKEN']
+rescue
+  # 設定ファイルがなかったら環境変数を読み込む
+    discord_token = ENV['DISCORD_TOKEN']
 end
+
 puts discord_token
 
 bot = Discordrb::Bot.new token: discord_token
