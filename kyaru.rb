5afe7d48d56123c4f268ecfc24f76f7422e5f5ac
satoki <satoki@applenoMacBook-Pro.local>
2019-09-16 23:19:53 +0900

    時給の概念簡易実装

--- kyaru.rb
+++ kyaru.rb
@@ -90,6 +90,8 @@ bot.heartbeat do |event|
   now = Time.new
   if previous.hour < now.hour then
     bot.send_message('613223157423276053', 'https://gyazo.com/0e4a0ca3bf8bcfd46cd14e078da3fbba')
+    bot.send_message '時給'
+    bot.send_message 'money+890'
     previous = now
   end
 end
