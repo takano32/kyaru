b85f14b2f0d9b95f289644c8e3f84c3a1087f1f3
platoronical <platoronical@gmail.com>
2019-09-16 23:41:13 +0900

    ルピ

--- kyaru.rb
+++ kyaru.rb
@@ -91,7 +91,7 @@ bot.heartbeat do |event|
   if previous.hour < now.hour then
     bot.send_message('613223157423276053', 'https://gyazo.com/0e4a0ca3bf8bcfd46cd14e078da3fbba')
     bot.send_message '時給です'
-    bot.send_message 'money+1000'
+    bot.send_message 'money+1000ルピ'
     previous = now
   end
 end
