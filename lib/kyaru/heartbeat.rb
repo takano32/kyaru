da2a8600d1e67274c7a555784226a5ce56172b03
yuiseki <yuiseki@gmail.com>
2019-09-18 13:43:33 +0900

    fix heartbeat.rb

--- lib/kyaru/heartbeat.rb
+++ lib/kyaru/heartbeat.rb
@@ -22,20 +22,23 @@ class Kyaru::Heartbeat
       # 1時間に1回やりたい処理
       now = Time.new
       if previous.hour < now.hour
+        @bot.send_message('613223157423276053', "#{now.hour}時になった")
         # 9時から22時まで働く
         if 9 < now.hour && now.hour < 22
+          @bot.send_message('613223157423276053', "キャルは働いている")
           # 時給を与える
           @money.set(:amount => @money.amount+hourly_wage)
           @money.save
           @bot.send_message('613223157423276053', "キャルは時給#{hourly_wage}円を得た")
-          # ストレスが溜まる
+          # 働くとストレスが溜まる
           @stress.set(:amount => @stress.amount+1)
           @stress.save
           @bot.send_message('613223157423276053', "キャルは#{@stress.amount}ストレスをためている")
         end
         # 23時から8時まで寝る
         if 23 < now.hour || now.hour < 8
-          # ストレスが減る
+          @bot.send_message('613223157423276053', "キャルは寝ている")
+          # 寝るとストレスが減る
           @stress.set(:amount => @stress.amount-1)
           @stress.save
           @bot.send_message('613223157423276053', "キャルのストレスが#{@stress.amount}になった")
